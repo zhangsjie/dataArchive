df404885a1561949ca53a17049306aa5c9071bf3
zhangsjie <sheng-jie.zhang@hpe.com>
2017-05-08 18:05:40 +0800

    dao2

--- src/main/java/com/hpe/dataArchive/Dao/DataBaseDao.java
+++ src/main/java/com/hpe/dataArchive/Dao/DataBaseDao.java
@@ -6,5 +6,5 @@ import java.util.List;
  * Created by zhangshe on 5/5/2017.
  */
 public interface DataBaseDao {
-    List<String> queryAll(String dataBaseName);
+    List<String> queryAll(String dataBaseName,String tableName);
 }
