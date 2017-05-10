2da96eed51052117d3d1af0c91d4914bf06ffde4
zhangsjie <sheng-jie.zhang@hpe.com>
2017-05-10 18:06:44 +0800

    同上

--- src/main/java/com/hpe/dataArchive/controller/DataBaseController.java
+++ src/main/java/com/hpe/dataArchive/controller/DataBaseController.java
@@ -2,6 +2,7 @@ package com.hpe.dataArchive.controller;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.hpe.dataArchive.service.DataBaseService;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,12 +26,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("dataBase")
 public class DataBaseController {
 
+	private DataBaseService dataBaseService;
+	
 	@RequestMapping(value = "/find.do", method = RequestMethod.POST)
 	public String searchDataBase(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		String databaseName=req.getParameter("databaseName");
 		String tableName=req.getParameter("tableName");
 		String date=req.getParameter("date");
 		System.out.println(databaseName+"  "+tableName+"时间"+date);
+		List<String> fields=dataBaseService.getFieldByDataBase(tableName);
 		
 		return "success";
 		
